f370271d962c820bb3e0fb92500fb3fec8bd3067
mmico <mico.maren@gmail.com>
2018-11-23 14:16:11 +0100

    Dopolnjen main menu

--- scripts/gameplay.js
+++ scripts/gameplay.js
@@ -4,3 +4,15 @@ function startGame(){
 	document.getElementById("mainMenu").style.display = 'none';
 	document.getElementById("glcanvas").style.display = 'inline';
 }
+
+function showCredits(){
+	document.getElementById("main").style.display = 'none';
+	document.getElementById("credits").style.display = 'inline';
+	document.getElementById("credits").style.width = '1280px';
+	document.getElementById("credits").style.height = '720px';
+}
+
+function goBack(){
+	document.getElementById("credits").style.display = 'none';
+	document.getElementById("main").style.display = 'inline';
+}
