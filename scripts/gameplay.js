827ad895ea1a624c616701b2b6132a534faa678d
mmico <mico.maren@gmail.com>
2018-11-23 17:14:36 +0100

    Dodan audio

--- scripts/gameplay.js
+++ scripts/gameplay.js
@@ -3,6 +3,14 @@ function startGame(){
 	gameStart = true;
 	document.getElementById("mainMenu").style.display = 'none';
 	document.getElementById("glcanvas").style.display = 'inline';
+	
+	var audio = new Audio("./assets/game.mp3");
+	audio.volume = 0.3;
+	audio.addEventListener('ended', function() {
+		this.currentTime = 0;
+		this.play();
+	}, false);
+	audio.play();
 }
 
 function showCredits(){
